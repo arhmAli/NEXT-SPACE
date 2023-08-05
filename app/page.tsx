f594271f1a42b7606ea03f43fbc7794134fc0f4d
Arham Khan <arhamkhank353@gmail.com>
2023-08-06 01:47:36 +0500

    page comp

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { getServerSession } from "next-auth"
+import {redirect} from 'next/navigation'
+
+export default function Home() {
+  const session=getServerSession()
+
+  if (!session){
+    redirect('/api/auth/signin')
+  }
+  return (
+    <main >
+
+    </main>
+  )
+}
